f7fefcf7aa0352a2ff3be1ad0ac618c08fc04e9f
Andante <pavel-andante@yandex.ru>
2025-04-15 23:05:20 +0300

    Fix

--- src/Providers/SeoServiceProvider.php
+++ src/Providers/SeoServiceProvider.php
@@ -12,9 +12,7 @@ final class SeoServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->publishes([
-            __DIR__ . '/../database/migrations' => database_path('migrations')
-        ], 'migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
 
         $this->app->make('router')
             ->pushMiddlewareToGroup('web', SeoMiddleware::class);
