10d826c7eea45b433f602d63d0dbbc112d8c0c75
Andante <pavel-andante@yandex.ru>
2025-07-09 14:00:10 +0300

    Fix

--- src/Http/Middleware/SeoMiddleware.php
+++ src/Http/Middleware/SeoMiddleware.php
@@ -5,17 +5,27 @@ namespace Gyrobus\MoonshineSeo\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Gyrobus\MoonshineSeo\Models\Seo;
+use Illuminate\Support\Facades\Storage;
 
 class SeoMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
         if ($request->isMethod('get')) {
+
             $seo = Seo::where('path', $request->path())->first();
+
             seo()
                 ->title($seo->title ?? env('APP_NAME'))
                 ->description($seo->description ?? env('APP_NAME'));
-            if ($seo && $seo->images) seo()->images($seo->images);
+
+            if ($seo && $seo->image) {
+                $storage = Storage::disk(config('moonshine-seo.image.disk', 'public'));
+                if ($storage->exists($seo->image)) {
+                    seo()->images($storage->url($seo->image));
+                }
+            }
+
         }
 
         return $next($request);
