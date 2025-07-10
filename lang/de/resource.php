e5344cab037212a0de70f82522ebfe549341c374
Andante <pavel-andante@yandex.ru>
2025-07-10 20:01:27 +0300

    Add language

---
+++ lang/de/resource.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'menu' => 'Seo',
+    'title' => 'Seo-Daten',
+    'fields' => [
+        'path' => 'Seiten-URL',
+        'title' => 'Titel',
+        'description' => 'Beschreibung',
+        'image' => 'Bild'
+    ]
+];
---
+++ lang/fr/resource.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'menu' => 'Seo',
+    'title' => 'Données Seo',
+    'fields' => [
+        'path' => 'URL de la page',
+        'title' => 'Titre',
+        'description' => 'Description',
+        'image' => 'Image'
+    ]
+];
